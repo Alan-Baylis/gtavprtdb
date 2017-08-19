6c504246d6838247fcbefa37a7bac50c10daef58
Qinka <qinka@live.com>
2017-08-19 11:24:35 +0800

    update ;

--- scripts/gtavprtdb-generate/src/GTAVPRTDB/Generate.hs
+++ scripts/gtavprtdb-generate/src/GTAVPRTDB/Generate.hs
@@ -18,13 +18,13 @@ generateSeed = (,,) <$> generateString <*> generatePlateI <*> generateLabel
   where generateString = take 8 <$> getRandoms >>=
           return . map (\i -> fontList !! mod i 37)
         generatePlateI = (`mod` 5) <$> getRandom
-        generateOffsetA = getRandomR (-90,90)
-        generateOffsetB = getRandomR (-180,180)
-        generateScale   = getRandomR (0.4,2)
+        generateOffsetA = nonLinearTrans1 90    <$> getRandomR (-90,90)
+        generateOffsetB = nonLinearTrans1 180   <$> getRandomR (-180,180)
+        generateScale   = nonLinearTrans2 0.4 2 <$> getRandomR (0.4,2)
         generateMoveX  = getRandomR (-480,480)
         generateMoveY  = getRandomR (-268,268)
         check a b s dx dy =
-          let preIs = a <= 30 && a >= (-30)
+          let preIs = a <= 45 && a >= (-45)
                       && b <= 60 && b >= (-60)
                       && s <= 1.45 && s >= 0.65
               off = generateOffset a b s
@@ -34,6 +34,8 @@ generateSeed = (,,) <$> generateString <*> generatePlateI <*> generateLabel
           in (if is move && preIs then 1 else 0) : move'
         generateLabel = check <$> generateOffsetA <*> generateOffsetB
          <*> generateScale  <*> generateMoveX <*> generateMoveY
+        nonLinearTrans1 base a = a ^ 5 / base ^ 4
+        nonLinearTrans2 b1 b2 a = nonLinearTrans1 (b2 - b1) a + b1
 
 
 
@@ -58,7 +60,7 @@ generateImg fp bgFiles = do
         loadBg n = loadAnyImg ImreadUnchanged  $ fp ++ '/' : n
 
 
-generateOffset :: Float -- ^ alpha |/  (for z axis) \in [-30,30]
+generateOffset :: Float -- ^ alpha |/  (for z axis) \in [-45,45]
                -> Float -- ^ beta  _/_ (for h(or x) axis \in [-60,60]
                -> Float -- ^ gamma \in [0.65,1.45]
                -> PointOffset Float
@@ -67,12 +69,22 @@ generateOffset a' b' g = [ V2 (-dH) (-dV)
                          , V2 (-dH)   dV
                          , V2   dH    dV
                          ]
-  where o al = 1 - cos al
-        h = 480
-        v = 268
-        dH = g * h * o a * cos b
-        dV = g * v * o a * sin b
-        a = a' / 180 * pi
-        b = b' / 180 * pi
+  where o al = 1 - sin al
+        f al = 1 + sin al
+        h = 256
+        v = 168
+        dLTX 
+        dLTY
+        dRTX
+        dRTY
+        dLBX
+        dLBY
+        dRBX
+        dRBY
+        dV = o a * sin b
+        a'' = a' / 180 * pi
+        b'' = b' / 180 * pi
+        a = a'' * cos b''
+        b = a'' * sin b''
 
 
